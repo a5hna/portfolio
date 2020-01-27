1f406a736dfb3cb49c66e6ec6934d57fdc5fdaf2
ashna shah <a5hna@hotmail.co.uk>
2020-01-27 11:50:41 +0000

    portfolio images are now landscape. Also styling changes to full site

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   get 'navigation', to: 'pages#navigation'
   get 'about', to: 'pages#about'
   get 'contact', to: 'pages#contact'
+  get 'travelpin', to: 'pages#travelpin'
+  get 'skuter', to: 'pages#skuter'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
