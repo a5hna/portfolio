fdfac75833d4b5b82d9c4001094fed11483bb4f4
ashna shah <a5hna@hotmail.co.uk>
2020-05-11 22:52:59 +0100

    precompiled images

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
   get 'contact', to: 'pages#contact'
   get 'travelpin', to: 'pages#travelpin'
   get 'skuter', to: 'pages#skuter'
+  get 'arthur', to: 'pages#arthur'
+  get 'lewagon', to: 'pages#lewagon'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
